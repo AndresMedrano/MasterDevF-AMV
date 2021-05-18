2bdbe91a61344ed25236888907da1af033f273ca
AndresMedrano <andres.medrano@softech.com.ec>
2021-05-18 09:55:48 -0500

    Primera Sesión Kata Js

---
+++ Kata_Js/01_intro_js/assets/js/conceptos.js
@@ -0,0 +1,48 @@
+// Inicio
+var x=10
+var y=8
+var z=12
+
+// Reasignacion
+x = y //11
+x = z //12
+
+
+//Comparando
+
+x == y //false
+
+//Diferente
+x != y //true
+
+//Menor
+
+y <= z
+
+//Strings
+
+var saludo = 'Mi nombre es '
+var nombre = 'Andres Medrano'
+
+var titulo = saludo + nombre
+
+alert(titulo)
+
+// Las Variables se pueden diferenciar entre mayusculas y minusculas
+
+// Palabras reservadas Consultar todas en 
+
+    // var
+    // true
+    // false
+
+// Boleanos
+
+var trueValue = true
+var falsValue = false
+
+console.log('Hola Devs ' + saludo + nombre + y)
+
+console.log(
+    'Ingresa a este link: https://campus.devf.la/'
+)
